f388a6faf8078cb911353a87a70a1726182be318
yash-joshi5379 <115107850+yash-joshi5379@users.noreply.github.com>
2025-04-25 21:10:00 +0100

    Add files via upload

--- player1.c
+++ player1.c
@@ -1,66 +1,170 @@
 #include "header.h"
+#include <stdio.h>
 
-// Tracks opponent's tendencies
-static int opponent_shoots_self = 0;
-static int opponent_shoots_other = 0;
 
-int player1_abstain(Player self, Player other, Gun state, int opponentRequest)
-{
-  // Accept if opponent requests AND we're at severe disadvantage
-  if (opponentRequest && (self.lives <= 1 && other.lives >= 3))
-  {
-    return 1; // Accept abstention as last resort
+/* ----------------------------------------
+The terminal commands for testing the bot can be found in the "cmds.txt" file.
+  Ensure the directory you are running the terminal in is correct or things will
+  not load correctly at runtime
+---------------------------------------- */
+
+int player1_abstain(Player self, Player other, Gun state, int opponentRequest) {
+  /* ----------------------------------------
+  This is a technically optional piece of logic. The abstention mechanic is
+    detailed in the game mechanics, but you have the option to not interact
+    at all with this system - in that case, simply leave this function
+    untouched.
+
+  If you do want to utilise this mechanic, return 1 to request an abstain,
+    or return 0 to deny.
+  If the "opponentRequest" value is 1, then your opponent has also requested
+    an abstain, and you are accepting or denying this. Otherwise, it is your
+    initiative to request an abstention.
+  ---------------------------------------- */
+
+  if (opponentRequest == 0){
+    return 0;
   }
-  return 0; // Default: play to win
+
+  int chance_of_death = ((float)(state.currentLiveBullets) / (float)(state.currentBullets)) * 100.0 ;
+
+  if (chance_of_death == 100){
+    return 0;
+  }
+
+  if (other.lives - self.lives >= 3){
+    return 1 ;
+  }
+
+  if (self.rounds == 1 && other.rounds == 0){
+    return 0 ;
+  }
+
+  if (self.rounds == 0 && other.rounds == 1){
+    return 1 ;
+  }
+
+  if (self.rounds == 0 && other.rounds == 0){
+    return 0 ;
+  }
+
+  if (self.rounds == 1 && other.rounds == 1){
+    return 0 ;
+  }
 }
+ItemAction player1_item(Player self, Player other, Gun state) {
+  /* ----------------------------------------
+  This is the first major function you will be modifying in order to create
+    your bot
 
-ItemAction player1_item(Player self, Player other, Gun state)
-{
-  float live_prob = (float)state.currentLiveBullets / state.currentBullets;
+  It is responsible for using your available items if you wish to do so - you
+   can return one of [ACTION_NoItem, ACTION_Item1, ACTION_Item2].
+  Your available items are visible at [self.item1] and [self.item2] (as well
+   as all other data for both yourself and opponent)
+  ---------------------------------------- */
 
-  // Use Health Kit at 1 HP
-  if (self.lives == 1 && (self.item1 == ITEM_HealthKit || self.item2 == ITEM_HealthKit))
-  {
-    return (self.item1 == ITEM_HealthKit) ? ACTION_Item1 : ACTION_Item2;
+  int chance_of_death = ((float)(state.currentLiveBullets) / (float)(state.currentBullets)) * 100.0 ;
+  
+
+  if (state.currentLiveBullets == 0 && self.item1 == ITEM_HealthKit){
+    return ACTION_Item1;
+  }
+  else if (state.currentLiveBullets == 0 && self.item2 == ITEM_HealthKit){
+    return ACTION_Item2;
   }
 
-  // If opponent is aggressive, skip their turn
-  if (opponent_shoots_other > 3 && (self.item1 == ITEM_SkipEnemy || self.item2 == ITEM_SkipEnemy))
-  {
-    return (self.item1 == ITEM_SkipEnemy) ? ACTION_Item1 : ACTION_Item2;
+  if (state.currentBullets == 2 && self.item1 == ITEM_SkipEnemy){
+    return ACTION_Item1;
+  }
+  else if (state.currentBullets == 2 && self.item2 == ITEM_SkipEnemy){
+    return ACTION_Item2;
   }
 
-  // Peek if bullet count is low (for bluffing)
-  if (state.currentBullets <= 3 && (self.item1 == ITEM_PeekCurrent || self.item2 == ITEM_PeekCurrent))
-  {
-    return (self.item1 == ITEM_PeekCurrent) ? ACTION_Item1 : ACTION_Item2;
+  if (state.currentBullets == state.currentLiveBullets && self.item1 == ITEM_SkipEnemy){
+    return ACTION_Item1;
+  }
+  else if (state.currentBullets == state.currentLiveBullets && self.item2 == ITEM_SkipEnemy){
+    return ACTION_Item2;
   }
 
-  // Eject if next bullet is likely live
-  if (live_prob > 0.6 && (self.item1 == ITEM_EjectCurrent || self.item2 == ITEM_EjectCurrent))
-  {
-    return (self.item1 == ITEM_EjectCurrent) ? ACTION_Item1 : ACTION_Item2;
+  if (self.lives == 1 && self.item1 == ITEM_HealthKit){
+    return ACTION_Item1;
+  }
+  else if (self.lives == 1 && self.item2 == ITEM_HealthKit){
+    return ACTION_Item2;
   }
 
-  return ACTION_NoItem; // Hold of no good items
+  if (chance_of_death == 100){
+    return ACTION_NoItem;
+  }
+
+  if (self.item1 == ITEM_PeekCurrent){
+    return ACTION_Item1;
+  }
+  else if (self.item2 == ITEM_PeekCurrent){
+    return ACTION_Item2;
+  }
+  else {
+    if (self.lives < other.lives && self.item1 == ITEM_HealthKit){
+      return ACTION_Item1;
+    }
+    else if (self.lives < other.lives && self.item2 == ITEM_HealthKit){
+      return ACTION_Item2;
+    }
+    else {
+      if (chance_of_death == 50 && self.item1 == ITEM_EjectCurrent){
+        return ACTION_Item1;
+      }
+      else if (chance_of_death == 50 && self.item2 == ITEM_EjectCurrent){
+        return ACTION_Item2;
+      }
+      else {
+        if (chance_of_death == 50 && self.item1 == ITEM_ResetGun){
+          return ACTION_Item1;
+        }
+        else if (chance_of_death == 50 && self.item2 == ITEM_ResetGun){
+          return ACTION_Item2;
+        }
+        else {
+          if (chance_of_death >= 66 && self.item1 == ITEM_SkipEnemy){
+            return ACTION_Item1;
+          }
+          else if (chance_of_death >= 66 && self.item2 == ITEM_SkipEnemy){
+            return ACTION_Item2;
+          }
+        }
+      }
+    }
+  }
 }
+TurnAction player1_action(Player self, Player other, Gun state, Bullet nextBullet) {
+  /* ----------------------------------------
+  This is the second major function you will be modifying in order to create
+    your bot
 
-TurnAction player1_action(Player self, Player other, Gun state, Bullet nextBullet)
-{
-  float live_prob = (float)state.currentLiveBullets / state.currentBullets;
+  It is responsible for deciding who to shoot. You can see your own and your
+   opponent's data, and the current total bullets as well as the total live
+   bullets in the [state] struct.
 
-  // bluff
-  if (nextBullet == BULLET_Blank)
+  If your previous action was [ITEM_PeekCurrent], the value of nextBullet will
+   be either [BULLET_Live | BULLET_Blank] depending on the observed bullet.
+   Otherwise, this value will be BULLET_UNKNOWN
+  ---------------------------------------- */
+
+  int chance_of_death = ((float)(state.currentLiveBullets) / (float)(state.currentBullets)) * 100.0 ;
+
+  if (nextBullet == BULLET_Live){
+   return ACTION_ShootOther;
+  }
+  else if (nextBullet == BULLET_Blank){
     return ACTION_ShootSelf;
-
-  // aggressive: If live_prob > 60%, shoot opponent
-  if (live_prob > 0.6f)
-    return ACTION_ShootOther;
-
-  // play safe If opponent is near death, shoot them
-  if (other.lives == 1)
-    return ACTION_ShootOther;
-
-  // default Shoot self if blank odds are good
-  return (live_prob < 0.5f) ? ACTION_ShootSelf : ACTION_ShootOther;
+  }
+  else {
+    if (chance_of_death >= 50){
+      return ACTION_ShootOther;
+    }
+    else {
+      return ACTION_ShootSelf;
+    }
+  }
 }
